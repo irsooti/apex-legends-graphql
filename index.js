9c3aa7e56bfcc9a55de30a3dc9d9ccc770087965
Daniele I <irsooti@gmail.com>
2019-03-01 15:31:17 +0100

    fix some aspects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').load();
 const { useRedis, cacheWithKey } = require('./cache');
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 const apexClient = require('./api/apexHttpClient');
 
 const resolvers = {
@@ -22,21 +22,41 @@ const resolvers = {
         id: child.id,
         legendName: child.metadata.legend_name,
         icon: child.metadata.icon,
-        bgImage: child.metadata.bgimage
+        bgImage: child.metadata.bgimage,
+        stats: child.stats
       }));
     }
   },
   Stats: {
     kills: parent => {
       const kills = parent.filter(stat => stat.metadata.key === 'Kills');
-      return kills.length ? kills[0] : null;
+      return kills.length ? kills[0] : { value: -1, percentile: -1 };
+    }
+  },
+  Subscription: {
+    counter: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random()
+          .toString(36)
+          .substring(2, 15); // random channel name
+        let count = 0;
+        setInterval(
+          () => pubsub.publish(channel, { counter: { count: count++ } }),
+          2000
+        );
+        return pubsub.asyncIterator(channel);
+      }
     }
   }
 };
 
+const pubsub = new PubSub();
 const server = new GraphQLServer({
   typeDefs: ['./src/schema.graphql'],
-  resolvers
+  resolvers,
+  context: { pubsub }
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000 🤘`));
+server.start(() =>
+  console.log(`Server is running on http://localhost:4000 🤘`)
+);
